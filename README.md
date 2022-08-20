8612bf708e45450cad6c83bf7506c540a148718d
abdo <abdoholmes97@gmail.com>
2022-08-20 13:54:26 +0200

    chore: INIT Commit & install react-fps

---
+++ README.md
@@ -0,0 +1,1 @@
+# List of customer tickets
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { FpsView } from 'react-fps';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='main-title'>Customer Tickets: Ticket</h1>
+      <FpsView />
+    </div>
+  );
+}
+
+export default App;
